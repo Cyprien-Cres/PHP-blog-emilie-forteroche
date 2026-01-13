17942baa2ffae0ba2b9240cf7d08bd06059dfe07
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2026-01-14 00:22:10 +0100

    Ajout icon de tri

--- views/templates/dashboard.php
+++ views/templates/dashboard.php
@@ -8,15 +8,27 @@
     <div class="dashboardLine">
         <a class="title sortable" href="<?= $sortUrls['title'] ?>">
             Titre de l'article
+            <?php if ($_GET['sort'] === 'title') : ?>
+                <span class="sortIcon"><?= $_GET['order'] === 'asc' ? '▲' : '▼' ?></span>
+            <?php endif; ?>
         </a>
         <a class="viewNumber titleColumn sortable" href="<?= $sortUrls['views'] ?>">
             Nombre de vues
+            <?php if ($_GET['sort'] === 'views') : ?>
+                <span class="sortIcon"><?= $_GET['order'] === 'asc' ? '▲' : '▼' ?></span>
+            <?php endif; ?>
         </a>
         <a class="commentNumber titleColumn sortable" href="<?= $sortUrls['comments'] ?>">
             Nombre de commentaires
+            <?php if ($_GET['sort'] === 'comments') : ?>
+                <span class="sortIcon"><?= $_GET['order'] === 'asc' ? '▲' : '▼' ?></span>
+            <?php endif; ?>
         </a>
         <a class="creationDate titleColumn sortable" href="<?= $sortUrls['date'] ?>">
             Date de création
+            <?php if ($_GET['sort'] === 'date') : ?>
+                <span class="sortIcon"><?= $_GET['order'] === 'asc' ? '▲' : '▼' ?></span>
+            <?php endif; ?>
         </a>
     </div>
     <?php foreach ($dashboards as $dashboard) : ?>
